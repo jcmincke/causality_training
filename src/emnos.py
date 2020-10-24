8d6db18060785f9eae229d1f7de4db3721010545
Jean-Christophe Mincke <jeanchristophe.mincke@gmail.com>
2020-10-24 19:07:56 +0200

    Add emnos formula test and IPW

--- src/emnos.py
+++ src/emnos.py
@@ -1,21 +1,18 @@
+
 import numpy.random as npr
 
 import pandas as pd
 import numpy as np
+import math
 
 from src.scm import *
 
 import statistics as s
 
-npr.choice([i for i in range(0, 21)], n)
 
-n = 10000
-
-s.mean(npr.random(n) * 1000 *(1+npr.random(n) * 0.2))
-
-p0 =  0.1
-p1 =  0.6
-p2 =  0.3
+p0 =  0.2
+p1 =  0.3
+p2 =  0.5
 def f_segment(n) : return lambda: npr.choice([0, 1, 2], p=[p0, p1, p2], size=n)
 #def f_segment(n) : return lambda: npr.choice([0, 1, 2], p=[0.3333, 0.3333, 0.3334], size=n)
 #def f_segment(n) : return lambda: npr.choice([0, 1, 2], p=[0, 0, 1], size=n)
@@ -24,19 +21,21 @@ def f_season(n): return lambda: npr.choice([0, 1], n)
 def f_promo(n): return lambda: npr.choice([0, 1], n)
 
 
-def f_promo_mult(n): return lambda: (1 + npr.random(n) * 0.2)
-def f_season_mult(n): return lambda: (1 + npr.random(n))
+def f_promo_mult(n): return lambda: (1 + npr.random(n) * 0.5)
+def f_season_mult(n): return lambda: (2 + npr.random(n))
 
 
 
 # (1 + npr.random(promo.size) * 0.2))
 
+p_promo = 0.8
+
 def f_dep_promo(segment):
     p = np.where((segment == 0),
-                 npr.choice([0, 1], p=[0.9, 0.1], size=segment.size),
+                 npr.choice([0, 1], p=[p_promo, 1-p_promo], size=segment.size),
                  np.where((segment == 1),
-                          npr.choice([0, 1], p=[0.3, 0.7], size=segment.size),
-                          npr.choice([0, 1], p=[0.5, 0.5], size=segment.size)))
+                          npr.choice([0, 1], p=[p_promo, 1-p_promo], size=segment.size),
+                          npr.choice([0, 1], p=[1-p_promo, p_promo], size=segment.size)))
 
     return p
 
@@ -69,7 +68,8 @@ def f_ca_season(ca, season, season_mult):
 
 def f_y(ca, season): return ca * (1 + season * 1)
 
-exo = ["s", "seg", "p", "ca_0", "ca_1", "ca_2", "smult", "pmult"]
+def f_z(y): return np.where(y > 300, 1, 0)
+
 
 def mk_scm(ns):
     return {
@@ -87,7 +87,8 @@ def mk_scm(ns):
 
         "ca": wrap_4(f_ca, "seg", "ca_0", "ca_1", "ca_2"),
         "ca_promo": wrap_3(f_ca_promo, "ca", "p", "pmult"),
-        "y": wrap_3(f_ca_season, "ca_promo", "s", "smult")
+        "y": wrap_3(f_ca_season, "ca_promo", "s", "smult"),
+        "z": wrap_1(f_z, "y")
 
     }
 
@@ -108,42 +109,91 @@ def do(scm, ctxs, key, value):
     return scm3
 
 
-def y_for_promo(scm, exo, ctxs, promo):
+def y_for_promo(scm, ctxs, promo):
 
     scm_p = do(scm, ctxs, "p", promo)
     ctxs_p = val_all(scm_p)
     pdf = pd.DataFrame(ctxs_p)
 
-    return pdf[(pdf.s == 1)]
+    return pdf
 
 
-def exact_uplift(scm, exo, ctxs):
-    pdf_1 = y_for_promo(scm, exo, ctxs, 1)
-    pdf_0 = y_for_promo(scm, exo, ctxs, 0)
+def exact_uplift(scm, ctxs):
+    pdf_1 = y_for_promo(scm, ctxs, 1)
+    pdf_0 = y_for_promo(scm, ctxs, 0)
 
     return s.mean(pdf_1.y) - s.mean(pdf_0.y)
+
+
+def smean(df):
+
+    if len(df) > 0:
+        return s.mean(df)
+    return 0
+
+def smd(treated_pdf, control_pdf, covar):
+
+    mt = s.mean(treated_pdf[covar])
+    mc = s.mean(control_pdf[covar])
+
+    st = s.stdev(treated_pdf[covar])
+    sc = s.stdev(control_pdf[covar])
+
+    #print(mt, st, mc, sc)
+
+    r = abs(mt - mc) / math.sqrt((st * st + sc * sc)/2)
+
+    return r
 
 
 def ri(pdf):
     return pdf.reset_index(drop=True)
 
-n = 500000
-scm = mk_scm(n)
-ctxs = val_all(scm)
-pdf = pd.DataFrame(ctxs)
 
-#print(s.mean(pdf[(pdf.s==0) & (pdf.p==1)].y))
+def uplift(pdf, ctxs, rs):
 
-eu = exact_uplift(scm, exo, ctxs)
+    print(rs)
+    eu = exact_uplift(scm, ctxs)
 
-print("exact uplift ", eu)
+    print("exact uplift ", eu)
+
+    u1, cov_p1 = effect(pdf, "p", 1, "y", rs)
+    u0, cov_p0 = effect(pdf, "p", 0, "y", rs)
+
+    print("adjustement", u1 - u0)
 
 
-part_during = pdf[(pdf.s==1) & (pdf.p==1) ].head(int(n/5)).sample(frac=1)
-non_part_during = pdf[(pdf.s==1) & (pdf.p==0) ].head(int(n/5)).sample(frac=1)
+    u1 = effect_iwp(pdf, "p", 1, "y", rs)
+    u0 = effect_iwp(pdf, "p", 0, "y", rs)
 
-part_before = pdf[(pdf.s==0) & (pdf.p==0) ].head(int(n/5)).sample(frac=1)
-non_part_before = pdf[(pdf.s==0) & (pdf.p==0) ].head(int(n/5)).sample(frac=1)
+    print("ipw", (u1 - u0))
+
+
+    n = 200000
+    scm = mk_scm(n)
+    ctxs = val_all(scm)
+    pdf = pd.DataFrame(ctxs)
+
+    rs = ["seg", "z"]
+    uplift(pdf, ctxs, rs)
+
+
+    rs = ["seg"]
+    uplift(pdf, ctxs, rs)
+
+rs = ["seg", "s"]
+uplift(pdf, ctxs, rs)
+
+
+
+
+
+nr = n #int(n/5)
+part_during = pdf[(pdf.p==1) ].head(int(nr)).sample(frac=1).copy()
+non_part_during = pdf[(pdf.p==0) ].head(int(nr)).sample(frac=1).copy()
+
+part_before = pdf[(pdf.s==0) & (pdf.p==0) ].head(nr).sample(frac=1)
+non_part_before = pdf[(pdf.s==0) & (pdf.p==0) ].head(nr).sample(frac=1)
 
 uplift = s.mean(part_during.y) - s.mean(non_part_during.y)
 
@@ -153,17 +203,64 @@ print(uplift)
 
 # seg 0
 
-uplift_0 = s.mean(part_during[part_during.seg == 0].y) - s.mean(non_part_during[non_part_during.seg == 0].y)
-uplift_1 = s.mean(part_during[part_during.seg == 1].y) - s.mean(non_part_during[non_part_during.seg == 1].y)
-uplift_2 = s.mean(part_during[part_during.seg == 2].y) - s.mean(non_part_during[non_part_during.seg == 2].y)
+
+
+uplift_0 = smean(part_during[part_during.seg == 0].y) - smean(non_part_during[non_part_during.seg == 0].y)
+uplift_1 = smean(part_during[part_during.seg == 1].y) - smean(non_part_during[non_part_during.seg == 1].y)
+uplift_2 = smean(part_during[part_during.seg == 2].y) - smean(non_part_during[non_part_during.seg == 2].y)
 
 uplift2 = uplift_0 * p0 + uplift_1 * p1 + uplift_2 * p2
 
 print(uplift2)
 
+q = smd(part_during, non_part_during, "seg")
 
-exos(scm)
+print("smd = ", q)
 
+
+# McNemar random test
+
+a = part_during[[]]
+
+a["y0"] = np.array((part_during.y))
+a["y1"] = np.array((non_part_during.y))
+
+
+def mix_it(a):
+    a["s"] = npr.choice([0, 1], nr)
+    a[["z0"]] = a.s * a.y0 + (1 - a.s) * a.y1
+    a[["z1"]] = (1 - a.s) * a.y0 + a.s * a.y1
+
+    d = s.mean(a.z0) - s.mean(a.z1)
+
+    return d
+
+l = [ mix_it(a) for _ in range(100)]
+
+print("mean = ", s.mean(l))
+
+print("stdev = ", s.stdev(l))
+
+a[["z0"]] = a.s * a.y0 + (1 - a.s) * a.y1
+a[["z1"]] = (1 - a.s) * a.y0 + a.s * a.y1
+
+
+a.insert(2, "s", list(npr.choice([0, 1], nr)))
+
+
+a.insert(0, "y0", list(part_during.y))
+a.insert(1, "y1", list(non_part_during.y))
+a.insert(2, "s", list(npr.choice([0, 1], nr)))
+
+a["z"] = list(npr.choice([0, 1], nr))
+
+a.insert(3, "z" =  a.y0 + a.y1
+
+switch = npr.choice([0, 1], nr)
+
+a = np.array(list(part_during.y) + list(non_part_during.y))
+
+len(part_during[part_during.seg == 0])
 # emnos
 
 k  = (s.mean(non_part_during.y) / s.mean(non_part_before.y))
--- src/scm.py
+++ src/scm.py
@@ -1,4 +1,11 @@
 
+
+
+import functools
+import statistics as s
+
+import pandas as pd
+
 def rule_for(scm, var: str):
     return scm[var][0]
 
@@ -29,7 +36,6 @@ def wrap_0(f):
 def wrap_1(f, var1):
     def f1(scm, ctx):
         v1 = val(scm, ctx, var1)
-        print(v1)
         return f(v1)
     return (f1, "endo")
 
@@ -38,7 +44,6 @@ def wrap_2(f, var1, var2):
     def f1(scm, ctx):
         v1 = val(scm, ctx, var1)
         v2 = val(scm, ctx, var2)
-        print(v1.size, v2.size)
         return f(v1, v2)
     return (f1, "endo")
 
@@ -80,3 +85,45 @@ def wrap_6(f, var1, var2, var3, var4, var5, var6):
         return f(v1, v2, v3, v4, v5, v6)
 
     return (f1, "endo")
+
+
+
+
+def effect(pdf, x, x_val, y, rs):
+
+    n = len(pdf.index)
+
+    cov_prob = pdf[rs].value_counts() / n
+
+    p0 = pdf[x] == x_val
+
+    def weigted_term(index):
+
+        preds = [pdf[r] == v for (r, v) in zip(rs, index)]
+
+        p = functools.reduce(lambda acc, p: acc & p, preds, p0)
+
+        return s.mean(pdf[p][y]) * cov_prob[index]
+
+    ws = functools.reduce(lambda a, b: a + b, [weigted_term(index) for index in cov_prob.index])
+
+    return (ws, cov_prob)
+
+
+def effect_iwp(pdf, x, x_val, y, rs):
+
+    pdfx = pdf[pdf[x] == x_val]
+
+    prs = pd.DataFrame(pdf[rs].value_counts()).rename(columns={0: "nrs"}, inplace=False)
+    psx = pd.DataFrame(pdfx[rs].value_counts()).rename(columns={0: "nx"}, inplace=False)
+
+
+    prop_pdf = pd.merge(prs, psx, right_index=True, left_index=True)
+    prop_pdf["prop"] = prop_pdf.nx / prop_pdf.nrs
+
+    a = pd.merge(pdfx, prop_pdf, right_index=True, left_on=rs)
+
+    a["py"] = a[y] / a.prop
+
+    return sum(a.py) / (len(pdf[y]))
+
